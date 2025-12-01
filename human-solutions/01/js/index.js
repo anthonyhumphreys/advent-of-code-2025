f27925ad577aa0decfcf51a3659957a25fcb72c9
Anthony Humphreys <contact@anthonyhumphreys.co.uk>
2025-12-01 22:55:52 +0000

    additional logic to solve pt2 of day 1 puzzle

--- human-solutions/01/js/index.js
+++ human-solutions/01/js/index.js
@@ -4,28 +4,55 @@ const input = readFileSync("../../../inputs/01.txt", "utf-8").trim();
 
 let dial = 50;
 
-let counter = 0;
+let part1Counter = 0;
+let part2Counter = 0;
+
+function countClicksOnZero(start, signedSteps) {
+  if (signedSteps === 0) {
+    return 0;
+  }
+
+  const startNorm = ((start % 100) + 100) % 100;
+
+  if (signedSteps > 0) {
+    const numClicks = signedSteps;
+    const firstZeroClick = startNorm === 0 ? 100 : 100 - startNorm;
+
+    if (firstZeroClick > numClicks) {
+      return 0;
+    }
+
+    return 1 + Math.floor((numClicks - firstZeroClick) / 100);
+  } else {
+    const numClicks = -signedSteps;
+    const firstZeroClick = startNorm === 0 ? 100 : startNorm;
+
+    if (firstZeroClick > numClicks) {
+      return 0;
+    }
+
+    return 1 + Math.floor((numClicks - firstZeroClick) / 100);
+  }
+}
 
 function parseLine(line) {
   const direction = line[0];
-  const steps = parseInt(line.slice(1));
+  const steps = parseInt(line.slice(1), 10);
+  const signedSteps = direction === "R" ? steps : -steps;
 
-  if (direction === "R") {
-    dial += steps;
-  } else if (direction === "L") {
-    dial -= steps;
-  }
+  part2Counter += countClicksOnZero(dial, signedSteps);
 
-  dial = (dial % 100 + 100) % 100;
+  dial = (dial + signedSteps) % 100;
+  dial = (dial + 100) % 100;
+
   if (dial === 0) {
-    counter++
+    part1Counter++;
   }
 }
 
-const lines = input.split("\n");
-
-for (const line of lines) {
+for (const line of input.split("\n")) {
   parseLine(line);
 }
 
-console.log(counter);
+console.log(part1Counter);
+console.log(part2Counter);
