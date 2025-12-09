5648306aadd87fed2e3ee67b6dda67e7c8f78293
Anthony Humphreys <contact@anthonyhumphreys.co.uk>
2025-12-09 13:04:06 +0000

    extend solution for part 2

--- human-solutions/03/js/index.js
+++ human-solutions/03/js/index.js
@@ -1,7 +1,8 @@
 // Each line = a 'bank'
-// Each line - find largest num that can be formed by concat
+// Each line - find  largest num that can be formed by concat
 // a certain number of digits in left-to-right order (DO NOT rearrange)
-// add the digits
+// Part 1: 2 digits per row
+// Part 2: 12 digits per row
 
 import { readFileSync } from "node:fs";
 
@@ -9,25 +10,43 @@ const input = readFileSync("../../../inputs/03.txt", "utf-8").trim();
 
 const inputAsArr = input.split('\n');
 
-function getTwoBiggestLeftToRight(row) {
-  let maxJoltage = -1;
-
-  for (let i = 0; i < row.length; i++) {
-    for (let j = i + 1; j < row.length; j++) {
-      const joltage = parseInt(`${row[i]}${row[j]}`);
-      if (joltage > maxJoltage) {
-        maxJoltage = joltage;
+function getBiggestLeftToRight(row, numDigits) {
+  let result = [];
+  let startIndex = 0;
+  
+  for (let pos = 0; pos < numDigits; pos++) {
+    const digitsRemaining = numDigits - pos - 1;
+    
+    const endIndex = row.length - digitsRemaining;
+    
+    let maxDigit = -1;
+    let maxIndex = -1;
+    
+    for (let i = startIndex; i < endIndex; i++) {
+      const digit = parseInt(row[i]);
+      if (digit > maxDigit) {
+        maxDigit = digit;
+        maxIndex = i; 
       }
     }
+    
+    result.push(maxDigit);
+    
+    startIndex = maxIndex + 1;
   }
-
-  return maxJoltage;
+  
+  return parseInt(result.join(''));
 }
 
+
 let sum = 0;
+let sum2 = 0
 
 inputAsArr.forEach(row => {
-  sum += getTwoBiggestLeftToRight(row);
+  sum += getBiggestLeftToRight(row, 2);
+  sum2 += getBiggestLeftToRight(row, 12);
 });
 
+
 console.log(sum);
+console.log(sum2);
